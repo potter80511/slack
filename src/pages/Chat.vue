f00b13fe0b4e9c00eecc41be0774e2a31b07155d
簡豪江 <potter80511@gmail.com>
2021-04-02 22:10:06 +0800

    加上chat及login的page，以及裝上bootstrap

---
+++ src/pages/Chat.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Chat page</h2>
+</template>
+
+<script>
+export default {
+  name: 'chat',
+}
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="jumbotron bg-primary text-white text-center">
+      <h2 class="lead display-3">#SLACK#</h2>
+      <p>Realtime communication at it's best!</p>
+    </div>
+    <div class="container-fluid">
+      <div class="row mt-5">
+        <div class="col text-center">
+          <button class="btn btn-outline-danger btn-lg">
+            Login with Google
+          </button>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col text-center">
+          <button class="btn btn-outline-info btn-lg">
+            Login with Twitter
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+}
+</script>
+
+<style lang="scss">
+.btn, .jumbotron {
+  border-radius: 0;
+}
+</style>
