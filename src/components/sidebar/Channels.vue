d777622ce4678da60db7354cd36ea8e0247f5b86
johnnychien <johnnychien@tvbs.com.tw>
2021-04-15 17:34:48 +0800

    渲染出channels

--- src/components/sidebar/Channels.vue
+++ src/components/sidebar/Channels.vue
@@ -3,72 +3,123 @@
     <!-- <button class="btn btn-outline-primary">Add Channel</button> -->
     <b-button v-b-modal.channelModal>Add Channel</b-button>
 
+    <!-- show channel list -->
+    <div class="mt-4">
+      <button
+        type="button"
+        class="list-group-item list-group-item-action"
+        v-for="channel in channels"
+        :key="channel.id"
+      >
+        {{ channel.name }}
+      </button>
+    </div>
+
     <!-- Modal -->
 
     <b-modal id="channelModal">
-      <template #modal-header="{ close }">
+      <template #modal-header>
         <h5 class="modal-title text-dark">Modal title</h5>
         <button
           type="button"
           class="close"
-          @click="close()"
+          @click="cancelAddChannel()"
         >
           <span aria-hidden="true">&times;</span>
         </button>
       </template>
       <form action="">
         <div class="form-group">
-          <input type="text" id="new_channel" name="new_channel" placeholder="Channel Name" class="form-control">
+          <input
+            type="text"
+            v-model="newChannel"
+            id="new_channel"
+            name="new_channel"
+            placeholder="Channel Name"
+            class="form-control"
+          />
         </div>
+        <!-- errors -->
+        <ul class="list-group" v-if="hasErrors">
+          <li
+            class="text-danger"
+            v-for="(error, index) in errors"
+            :key="'error-' + index"
+          >
+            {{ error }}
+          </li>
+        </ul>
       </form>
-      <template #modal-footer="{ ok, cancel }">
+      <template #modal-footer>
         <button
           type="button"
           class="btn btn-secondary"
-          @click="cancel()"
+          @click="cancelAddChannel()"
         >
           Cancel
         </button>
         <button
           type="button"
           class="btn btn-primary"
-          @click="ok()"
+          @click="addChannel()"
         >
           Add Channel
         </button>
       </template>
     </b-modal>
-    <!-- <div class="modal fade" id="channelModal">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-dark">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-
-          <div class="modal-body">
-            <form action="">
-              <div class="form-group">
-                <input type="text" id="new_channel" name="new_channel" placeholder="Channel Name" class="form-control">
-              </div>
-            </form>
-          </div>
-
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-primary">Add Channel</button>
-          </div>
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
 
 <script>
+import firebase from 'firebase/app';
 
 export default {
   name: 'channels',
+  data () {
+    return {
+      newChannel: '',
+      errors: [],
+      channelsRef: firebase.database().ref('channels'),
+      channels: [],
+    }
+  },
+  mounted () {
+    this.addListeners();
+  },
+  computed: {
+    hasErrors() {
+      return this.errors.length > 0
+    },
+  },
+  methods: {
+    cancelAddChannel () {
+      this.newChannel = '';
+      this.errors = [];
+      this.$bvModal.hide('channelModal')
+    },
+
+    addChannel () {
+      const key = this.channelsRef.push().key;
+      const newChannel = { id: key, name: this.newChannel };
+
+      this.channelsRef.child(key).update(newChannel)
+        .then(() => {
+          this.cancelAddChannel();
+        }).catch((error) => {
+          this.errors.push(error.message);
+        });
+    },
+    addListeners () {
+      this.channelsRef.on('child_added', snapshot => {
+        this.channels.push(snapshot.val());
+      });
+    },
+    detachListeners() {
+      this.channelsRef.off();
+    },
+  },
+  beforeDestroy() {
+    this.detachListeners();
+  },
 }
 </script>
